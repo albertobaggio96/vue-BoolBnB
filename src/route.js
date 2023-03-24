689d1b1bbeb480ca3c29119f35fa8f5539a19065
albertobaggio96 <albertobaggio96@gmail.com>
2023-03-24 15:19:26 +0100

    create a new page route

--- src/route.js
+++ src/route.js
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomePage from "./pages/HomePage.vue";
 import PropertyShow from "./pages/PropertyShow.vue";
 import SearchPage from "./pages/SearchPage.vue";
+import AllPropertyPage from "./pages/AllPropertyPage";
 
 const router = createRouter({
     history: createWebHistory(),
@@ -21,6 +22,11 @@ const router = createRouter({
             path: '/search',
             name: 'search',
             component: SearchPage
+        },
+        {
+            path: '/all',
+            name: 'all',
+            component: AllPropertyPage
         },
     ]
 })
