d65a2087f76f1d7c2c7630a2fe9a2f26f4a24ea8
MyUsername <my@email.com>
2023-03-27 17:26:43 +0200

    Add style for home page

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,39 +1,57 @@
 <script>
-    import RouteInput from '../components/HomePage/RouteInput.vue'
-    import {store} from '../store'
-    export default {
-        name: 'HomePage',
-        components:{
-            RouteInput
-        }, 
-        data(){
-            return{
-                store,
-            }
-        },
-        methods:{
-            searchApparament(address){
-                this.store.storeParams.address = address;
-            },
-            resetStore(){
-                this.store.storeParams = {
-                    address : ''
-                }
-            }
-        },
-        created(){
-            this.resetStore()
+import RouteInput from '../components/HomePage/RouteInput.vue'
+import { store } from '../store'
+export default {
+    name: 'HomePage',
+    components: {
+        RouteInput
+    },
+    data() {
+        return {
+            store,
         }
-
+    },
+    methods: {
+        searchApparament(address) {
+            this.store.storeParams.address = address;
+        },
+        resetStore() {
+            this.store.storeParams = {
+                address: ''
+            }
+        }
+    },
+    created() {
+        this.resetStore()
     }
+
+}
 </script>
 
 <template>
-    <main>
-        <RouteInput @address="searchApparament"/>
+    <div class="container d-flex justify-content-center">
+        <div class="row">
+            <div class="col-12">
+                <img class="img-fluid" src="../assets/BoolB_B_logo-removebg-preview.png" alt="">
+            </div>
+        </div>
+    </div>
+
+    <main class="d-flex justify-content-center">
+        <RouteInput @address="searchApparament" />
     </main>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center fw-bold fs-1 m-3">
+                Ecco le nostre proposte!
+            </div>
+        </div>
+    </div>
+
+
 </template>
 
 <style lang="scss">
-    
-</style>
+
+    </style>
