29f813cccfecbca4d13eaaea7c9bc50d2af3a82a
cerry24 <samuelecerretti@gmail.com>
2023-03-27 16:54:14 +0200

    api test for single property show

--- src/pages/AllPropertyPage.vue
+++ src/pages/AllPropertyPage.vue
@@ -1,24 +1,53 @@
 <script>
 import {store} from '../store.js'
 import { computed } from 'vue';
+import axios from 'axios'
+
+import CardProperty from '../components/shered-components/CardProperty.vue';
 
 export default {
   name: 'AllPropertyPage',
+
+  components: {
+    CardProperty
+  },
+
   data(){
     return{
-      
+      store,
+      property: null
     }
   },
+
   methods: {
-    
+    getProperties() {
+      axios.get('http://127.0.0.1:8000/api/properties/accogliente-cottage-immerso-nella-natura-15', {
+        params: {
+        }
+      })
+      .then((response) => {
+        console.log(response)
+        this.property = response.data.results
+      })
+      .catch(function (error) {
+        console.warn(error);
+    });
+    }
   },
+
   created() {
+    this.getProperties()
   },
 }
 </script>
 
 <template>
   <main>
+    <div class="container">
+      <div class="row">
+          <CardProperty :property="property" class="col-10" />
+      </div>
+    </div>
   </main>
 </template>
 
