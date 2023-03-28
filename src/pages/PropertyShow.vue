dace11d6705aaff760d70dcde764a7ead8426c15
LorenzoOgnibene <lorenzo.ognibene@gmail.com>
2023-03-28 23:49:40 +0200

    added modal with all imgs and added resonsive layout

--- src/pages/PropertyShow.vue
+++ src/pages/PropertyShow.vue
@@ -1,7 +1,8 @@
 <script>
 import {store} from '../store.js'
 import axios from 'axios'
-import SendMessage from '../components/PropertyShow/SendMessage.vue'
+
+
 export default {
     name: 'PropertyShow',
     components:{
@@ -26,10 +27,11 @@ export default {
             .finally(function () {
                // always executed
             });  
-        }
+        },
     },
     created() {
         this.getPropertyApi()
+        this.maps()
     },
 }
 </script>
@@ -49,6 +51,7 @@ export default {
     <section>
         <div class="container">
             <div class="row">
+                <!-- CAROUSEL VISIBLE ONLY IN MOBILE AND TABLE -->
                 <div class="col-12 d-lg-none">
                     <div :id="'carouselExampleIndicators'+ property.id" class="carousel slide" data-bs-ride="false">
                         <div class="carousel-indicators">
@@ -77,13 +80,35 @@ export default {
                         </button>
                     </div>
                 </div>
+                <!-- LAYOUT FOR IMG VISIBILE ONLY IN DESKTOP MODE -->
                 <div class="col-6 p-1 d-none d-lg-inline-flex">
                     <img :src="'http://127.0.0.1:8000/storage/'+property.cover_img" alt="" class="img-fluid h-100 top-img">
                 </div>
             
-                <div class="col-6 h-100 d-flex flex-wrap p-1 d-none d-lg-inline-flex">
+                <div class="col-6 h-100 d-flex flex-wrap p-1 d-none d-lg-inline-flex position-relative">
                     <div class="wrapper-img" v-for="image, index in property.images.slice(1, 5)" :class="'wrapper'+index">
-                        <img :src="'http://127.0.0.1:8000/storage/'+image.path" class="img-fluid h-100" :class="'custom-img'+index" alt="">
+                        <img :src="'http://127.0.0.1:8000/storage/'+image.path" class="img-fluid h-100 test" :class="'custom-img'+index" alt="">
+                    </div>  
+                    <button type="button" class="btn button-outline" data-bs-toggle="modal" data-bs-target="#exampleModal"><font-awesome-icon icon="fa-solid fa-maximize" /> Mostra tutte le foto</button>
+                </div>
+                
+                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-fullscreen">
+                        <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body container">
+                            <div class="row justify-content-center">
+                                <div class="img-wrapper-modal col-12 col-lg-8 mb-1"  v-for="image in property.images">
+                                    <img :src="'http://127.0.0.1:8000/storage/'+image.path" alt="" class="img-fluid">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn button-secondary" data-bs-dismiss="modal">Close</button>
+                        </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -93,7 +118,7 @@ export default {
     <section>
         <div class="container">
             <div class="row">
-                <div class="col-8">
+                <div class="col-12 col-lg-8">
                     <div class="row">
                         <div class="col-12 my-3">
                             <p>{{property.n_beds}} Ospiti | {{property.n_rooms}} Camere da letto | {{ property.n_beds }} Letti | {{property.n_toilettes}} Bagni</p>
@@ -113,13 +138,11 @@ export default {
                             </div>
                         
                     </div>
+                    <hr>
                 </div>
-
-                <div class="col-4">
+                
+                <div class="col-12 col-md-8 offset-md-3 col-lg-4 offset-lg-0">
                     <div class="row align-content-center h-100 my-4">
-                        <!-- <div class="col-12">
-                            <p>Costo per notte: {{ property.night_price }}€</p>
-                        </div> -->
                         <div class="col-12">
                             <div class="card w-100">
                                 <div class="card-body">
@@ -127,14 +150,12 @@ export default {
                                         <h5 class="card-title">Costo <strong>{{ property.night_price }}€</strong> per notte</h5>
                                     </div>
                                     <hr>
-
                                     <SendMessage/>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-
             </div>
         </div>
     </section>
@@ -143,6 +164,7 @@ export default {
 
 
 <style lang="scss" scoped>
+    @use "@tomtom-international/web-sdk-maps" as*; 
     @use '../style/partials/variables' as*;
     .top-img{
         border-top-left-radius: 10px;
@@ -171,6 +193,16 @@ export default {
         border-bottom-right-radius: 10px;
     }
 
+    .button-outline{
+        position: absolute;
+        right: 15px;
+        bottom: 10px;
+        @include button-outline;
+    }
+
+    .button-secondary{
+        @include button-secondary;
+    }
     .card{
         border-radius: 20px;
         background: #847EF0;
@@ -179,6 +211,16 @@ export default {
         -webkit-box-shadow: 10px 10px 5px -1px rgba(0,0,0,0.75);
         -moz-box-shadow: 10px 10px 5px -1px rgba(0,0,0,0.75);
         box-shadow: 10px 10px 5px -1px rgba(0,0,0,0.75);
+    }
+
+    //REMOVE VERTICAL SCROLBAR FOR CHROME OPERA AND SAFARI
+    .modal-body::-webkit-scrollbar {
+        display: none;
+    }
+    //REMOVE VERTICAL SCROLBAR FOR EDGE AND FIREFOX
+    .modal-body {
+        -ms-overflow-style: none;  /* IE and Edge */
+        scrollbar-width: none;  /* Firefox */
     }
   
 </style>
