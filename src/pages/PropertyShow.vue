9b848ebab4acab8830f533c187843ea6e95ff881
LorenzoOgnibene <lorenzo.ognibene@gmail.com>
2023-03-29 11:51:35 +0200

    edit price on show

--- src/pages/PropertyShow.vue
+++ src/pages/PropertyShow.vue
@@ -136,11 +136,14 @@ export default {
             <div class="row">
                 <div class="col-12 col-lg-8">
                     <div class="row">
-                        <div class="col-12 my-3">
+                        <div class="col-12 col-md-6 my-3">
                             <p>{{property.n_beds}} Ospiti | {{property.n_rooms}} Camere da letto | {{ property.n_beds }} Letti | {{property.n_toilettes}} Bagni</p>
                         </div>
+                        <div class="col-12 col-md-6 text-md-end title mt-3 mb-5">
+                            <h5 class="card-title">Costo <strong>{{ property.night_price }}€</strong> per notte</h5>
+                        </div>
                         <hr>
-                        <div class="col-12 my-4">
+                        <div class="col-md-12 my-4">
                             <h3>Descrizione:</h3>
                             <p>{{ property.description }}</p>
                         </div>
@@ -170,8 +173,8 @@ export default {
                         <div class="col-12">
                             <div class="card w-100">
                                 <div class="card-body">
-                                    <div class="title mt-3 mb-5">
-                                        <h5 class="card-title">Costo <strong>{{ property.night_price }}€</strong> per notte</h5>
+                                    <div class="title mt-3 mb-5 text-center">
+                                        <h5 class="card-title">Invia un messaggio all'Host</h5>
                                     </div>
                                     <hr>
 
