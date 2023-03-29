af83f9023dd64caad8cefd5d2ea345c7b58db27d
MyUsername <my@email.com>
2023-03-29 16:29:58 +0200

    add logo

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -1,35 +1,39 @@
 <script>
-    export default {
-        name: 'navbar',
-    }
+export default {
+    name: 'navbar',
+}
 </script>
 <template>
     <div>
         <nav class="navbar navbar-expand-lg bg-light">
             <div class="container">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <a class="navbar-brand" href="#">
+                    <img src="../assets/Bool_icon.png" alt="Logo" width="30" height="24"
+                        class="d-inline-block align-text-top">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <router-link :to="{ name: 'home'}" class="nav-link"> home </router-link>
-                        </li>
-                        <!-- <li class="nav-item">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <router-link :to="{ name: 'home' }" class="nav-link"> home </router-link>
+                    </li>
+                    <!-- <li class="nav-item">
                             <router-link :to="{ name: 'show'}" class="nav-link"> show </router-link>
                         </li>
                         <li class="nav-item">
-                            <router-link :to="{ name: 'search'}" class="nav-link"> results </router-link>
-                        </li>
-                        <li class="nav-item">
-                            <router-link :to="{ name: 'all'}" class="nav-link"> all </router-link>
-                        </li> -->
+                                <router-link :to="{ name: 'search'}" class="nav-link"> results </router-link>
+                            </li>
+                            <li class="nav-item">
+                                <router-link :to="{ name: 'all'}" class="nav-link"> all </router-link>
+                            </li> -->
                     </ul>
                 </div>
             </div>
         </nav>
     </div>
 </template>
-<style lang="scss">
-    
-</style>
+<style lang="scss"></style>
