040f7e3fee81ee8acbc07109df90a2ffce59233d
LorenzoOgnibene <lorenzo.ognibene@gmail.com>
2023-03-29 20:44:31 +0200

    edit message

--- src/components/PropertyShow/SendMessage.vue
+++ src/components/PropertyShow/SendMessage.vue
@@ -42,15 +42,15 @@ export default {
           <form class="text-center">
               <div class="mb-3">
                   <label for="mail-guest" class="form-label">Inserisci la tua Email</label>
-                  <input class="form-control" type="mail" id="mail-guest" name="mail-guest" v-model="mailGuest" required minlength="2" maxlength="20">
+                  <input class="form-control" type="mail" id="mail-guest" name="mail-guest" v-model="mailGuest" required minlength="2" maxlength="100">
               </div>
               <div class="mb-3">
                   <label for="name-guest" class="form-label">Nome</label>
-                  <input class="form-control" type="text" id="name-guest" name="name-guest" v-model="nameGuest" required minlength="2" maxlength="20">
+                  <input class="form-control" type="text" id="name-guest" name="name-guest" v-model="nameGuest" required minlength="2" maxlength="100">
               </div>
               <div class="mb-3">
                   <label for="subject-guest" class="form-label">Oggetto del messaggio</label>
-                  <input class="form-control" type="text" id="subject-guest" name="subject-guest" v-model="subjectGuest" required minlength="2" maxlength="30">
+                  <input class="form-control" type="text" id="subject-guest" name="subject-guest" v-model="subjectGuest" required minlength="2" maxlength="100">
         
               </div>
               <div class="mb-3">
