6f5387f525ab5822a583d79a175deda3325a78bf
LorenzoOgnibene <lorenzo.ognibene@gmail.com>
2023-03-31 17:33:34 +0200

    edit nav bar and home layout

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -17,18 +17,18 @@ export default {
                 <div class="col-4">
                     <a class="navbar-brand ms-3" href="http://localhost:5174/"> <img src="../assets/Bool_icon.png" alt="Logo" width="30" height="24" class="d-inline-block me-auto"></a>
                 </div>
-                <div class="navbar-collapse col-4 text-center" id="navbarText">
-                    <ul class="navbar-nav me-auto">
-                        <li class="nav-item button-nav">
-                            <router-link :to="{ name: 'home' }" class="nav-link color-a fw-bold"> <i class="fa-solid fa-house"></i> <font-awesome-icon icon="fa-solid fa-house" class=""/><span class="d-none d-md-inline"> Home</span> </router-link>
+                <div class="navbar-collapse col-4" id="navbarText">
+                    <ul class="navbar-nav m-auto">
+                        <li class="nav-item button-nav px-3 py-2">
+                            <router-link :to="{ name: 'home' }" class="text-decoration-none color-a fw-bold"> <i class="fa-solid fa-house"></i> <font-awesome-icon icon="fa-solid fa-house" class=""/><span class="d-none d-md-inline"> Home</span> </router-link>
                         </li>
-                        <li v-if="store.getBack == 'search'" class="nav-item button-nav me-3">
-                            <router-link :to="{ name: store.getBack }" class="nav-link color-a fw-bold"> <i class="fa-solid fa-house"></i><font-awesome-icon icon="fa-solid fa-arrow-left" /></router-link>
+                        <li v-if="store.getBack == 'search'" class="nav-item button-nav px-3 py-2 ms-3">
+                            <router-link :to="{ name: store.getBack }" class="text-decoration-none color-a fw-bold"> <i class="fa-solid fa-house"></i><font-awesome-icon icon="fa-solid fa-arrow-left" /></router-link>
                         </li>
                     </ul>  
                 </div>
                 <div class="col-4 text-end">
-                    <span class="navbar-text button-nav">
+                    <span class="navbar-text button-nav px-3 py-2">
                         <a class="color-a text-decoration-none fw-bold" href="http://127.0.0.1:8000/admin"><font-awesome-icon icon="fa-solid fa-user" /> <span class="d-none d-md-inline"> Accedi</span></a>
                     </span>
                 </div>
@@ -38,13 +38,13 @@ export default {
 </template>
 <style lang="scss">
 @use '../style/partials/variables' as*;
-    .custom-nav{  
-        height: 60px;
+    nav{
+        border-bottom: 1px solid rgb(233, 224, 224);
     }
+
     .button-nav{
         border: 1px solid $secondary-purple;
         border-radius: 10px;
-        padding: 0 1rem;
         &:hover{
             background-color: $secondary-purple;
             .color-a{
--- src/components/HomePage/RouteInput.vue
+++ src/components/HomePage/RouteInput.vue
@@ -38,7 +38,7 @@ export default {
       addressLabel.after(searchBoxHTML)
       var inputAddress = document.getElementsByClassName('tt-search-box-input')[0]
       const addressBox = document.getElementsByClassName('tt-search-box')[0]
-      addressBox.classList.add('col-12', 'col-md-6')
+      addressBox.classList.add('col-9', 'col-md-6', 'col-lg-5', 'offset-lg-2')
       inputAddress.classList.add('form-control')
       inputAddress.setAttribute('name', 'inputAddress')
       inputAddress.setAttribute('autocomplete', 'off')
@@ -63,16 +63,15 @@ export default {
 <template>
   <div class="container">
     <div class="row  justify-content-center">
-      <div class="col-12 col-lg-6 col-md-9 text-center">
+      <div class="col-12 col-lg-6 col-md-9 offs text-center">
           <h1>Scegli una destinazione per le tue vacanze!</h1>
           <font-awesome-icon icon="fa-solid fa-arrow-down" />
       </div>
     </div>
-          <article id="address-input" class="row align-items-center">
-            <label for="address" id="address-label" class="d-none d-lg-inline"></label>
-            <router-link id="address-button" @click="getValue(), $emit('address', inputAddress)" :to="{ name: 'search' }"
-            class="btn button-primary col-12 col-md-6"> Invia </router-link>
-          </article>
+    <article id="address-input" class="row align-items-center">
+      <label for="address" id="address-label" class="d-none d-lg-inline offse"></label>
+      <router-link id="address-button" @click="getValue(), $emit('address', inputAddress)" :to="{ name: 'search' }" class="btn button-primary col-3 col-md-6 col-lg-2"> Invia </router-link>
+    </article>
   </div>
 </template>
 
@@ -80,14 +79,9 @@ export default {
 @use '../../style/partials/variables' as*;
 @import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Koulen&family=Lato&family=Nunito&family=Playfair+Display:ital@1&family=Prata&family=Raleway:ital,wght@1,100&family=Roboto&family=Roboto+Condensed&family=Teko&display=swap');
 #address-input {
-  //margin: 6rem;
   background-color: rgba(0, 0, 0, 0.1);
   padding: 1rem;
   border-radius: 2rem;
-
-  label {
-    //margin: .5rem;
-  }
 
   input {
     border-radius: 1rem;
@@ -96,35 +90,9 @@ export default {
 
 .button-primary{
   @include button-primary;
+  border-radius: 20px;
 }
-  // .btn {
-
-  //   font-family: Roboto, sans-serif;
-  //   font-weight: 0;
-  //   font-size: 14px;
-  //   color: #fff;
-  //   background-color: #6962e9;
-  //   padding: 10px 30px;
-  //   border: 2px solid #6962e9;
-  //   box-shadow: rgb(0, 0, 0) 0px 0px 0px 0px;
-  //   border-radius: 50px;
-  //   transition: 1000ms;
-  //   transform: translateY(0);
-  //   align-items: center;
-  //   cursor: pointer;
-
-  // }
-
-  // .btn:hover {
-
-  //   transition: 1000ms;
-  //   padding: 10px 50px;
-  //   transform: translateY(-0px);
-  //   background-color: #fff;
-  //   color: #0066cc;
-  //   border: solid 2px #0066cc;
-  // }
-
+  
   .tt-search-box-close-icon {
     display: none;
   }
@@ -134,8 +102,4 @@ export default {
   }
 }
 
-
-#address-button{
-  //margin-left: 1rem;
-}
 </style>
